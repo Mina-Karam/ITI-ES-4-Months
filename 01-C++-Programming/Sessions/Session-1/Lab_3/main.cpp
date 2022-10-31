c7c4699158f10bdf9fe165a7da74e04fe82cf7a6
Mina Karam <minakaram.me@gmail.com>
2022-10-31 18:40:42 +0200

    01-C++-Programming/Sessions/Session-1/Lab_3/main: create

---
+++ 01-C++-Programming/Sessions/Session-1/Lab_3/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    int x = 0; 
+
+    do
+    {
+        x++;
+        cout << x << " ";
+    } while (x <= 3);
+    
+    return 0;
+}
