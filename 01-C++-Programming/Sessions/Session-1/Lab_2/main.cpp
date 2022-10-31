805b662232f5b648bb0f34f6a0b2113d490d2eb0
Mina Karam <minakaram.me@gmail.com>
2022-10-31 18:32:32 +0200

    01-C++-Programming/Sessions/Session-1/Lab_2/main: create

---
+++ 01-C++-Programming/Sessions/Session-1/Lab_2/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    int x = 0; 
+
+    do
+    {
+        cout << x << " ";
+        x++;
+    } while (x <= 10);
+    
+    return 0;
+}
