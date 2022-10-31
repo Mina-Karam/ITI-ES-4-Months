b36a3d76196ea44c58beafb6984d8429c8c6b1ea
Mina Karam <minakaram.me@gmail.com>
2022-10-31 21:00:40 +0200

    01-C++-Programming/Sessions/Session-1/Lab_8/main: create

---
+++ 01-C++-Programming/Sessions/Session-1/Lab_8/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    char arr[13] = "Hello, World";
+
+    for (int i = 0; i < 13; i++)
+    {
+        cout << arr[i];
+    }
+    
+    return 0;
+}
