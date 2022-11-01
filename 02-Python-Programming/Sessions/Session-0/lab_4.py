409fd1ca608716d7d18b3dccae781a5a0b14d995
Mina Karam <minakaram.me@gmail.com>
2022-11-01 21:25:21 +0200

    02-Python-Programming/Sessions/Session-0/lab_4: add multiple code

--- 02-Python-Programming/Sessions/Session-0/lab_4.py
+++ 02-Python-Programming/Sessions/Session-0/lab_4.py
@@ -4,3 +4,27 @@ def myfunc():
     print("Python is " + x)
 
 myfunc()
+
+print("===========================")
+print("===========================")
+
+x = "awesome"
+
+def myfunc():
+    x = "fantastic"
+    print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
+
+print("===========================")
+print("===========================")
+
+def myfunc():
+    global x
+    x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
