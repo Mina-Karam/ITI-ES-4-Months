8f4521d123f12b87bd042953a1859fd5c9a5ad7f
Mina Karam <minakaram.me@gmail.com>
2022-11-01 21:44:30 +0200

    02-Python-Programming/Sessions/Session-0/lab_6: create

---
+++ 02-Python-Programming/Sessions/Session-0/lab_6.py
@@ -0,0 +1,25 @@
+# Arithmatic operators 
+
+a = 90
+b = 20
+
+#additaddition 
+print(a+b)
+
+#subtraction
+print(a-b)
+
+#multiplication
+print(a*b)
+
+#divison
+print(a/b)
+
+#Exponentiation
+print(a**b)
+
+#floor divison
+print(a//b)
+
+#Modulas
+print(a%b)
