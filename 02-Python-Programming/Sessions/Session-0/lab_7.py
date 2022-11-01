e9c4e271ca8379e993dd2e6f07d283a9fa234975
Mina Karam <minakaram.me@gmail.com>
2022-11-01 21:45:04 +0200

    02-Python-Programming/Sessions/Session-0/lab_7: create

---
+++ 02-Python-Programming/Sessions/Session-0/lab_7.py
@@ -0,0 +1,10 @@
+# relational operators 
+a = 5
+b = 10
+
+print(a == b)  #return False
+print(a != b)  #return True
+print(a < b)   #return True
+print(a <= b)  #return True
+print(a > b)   #return False
+print(a >= b)  #return False
