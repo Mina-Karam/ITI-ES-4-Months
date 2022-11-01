16e15c7e7d963aaee09770dc1b35d18fb5c1c64f
Mina Karam <minakaram.me@gmail.com>
2022-11-01 21:03:27 +0200

    Update Files

---
+++ 02-Python-Programming/Sessions/Session-0/lab_2.py
@@ -0,0 +1,11 @@
+"""
+YourName = input("Please Enter Your Name: ")
+YourAge = input("Please Enter Your Age: ")
+Yourfaculity = input("Please Enter Your facaulity: ") 
+"""
+
+
+
+print("Your name is: "+ YourName)
+print("Your Age is: "+ YourAge)
+print("Your facaulity is: "+ Yourfaculity)
