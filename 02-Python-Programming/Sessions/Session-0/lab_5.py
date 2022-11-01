6ed0db9150bc5a55de06b00e3fa696e373f114b5
Mina Karam <minakaram.me@gmail.com>
2022-11-01 21:41:29 +0200

    02-Python-Programming/Sessions/Session-0/lab_5: create

---
+++ 02-Python-Programming/Sessions/Session-0/lab_5.py
@@ -0,0 +1,14 @@
+'''
+Python does not have a random() function to make a random number,
+but Python has a built-in module called random 
+that can be used to make random numbers:
+'''
+
+import random
+
+print(random.randrange(1,10,2))
+
+'''
+Python has a built-in module that you can use to make random numbers.
+The random module has a set of methods: https://www.w3schools.com/python/module_random.asp
+'''
