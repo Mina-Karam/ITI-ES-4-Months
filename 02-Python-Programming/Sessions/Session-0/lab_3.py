3b69d7d540a66a3c2e6ba91b05a8bc04d6835b83
Mina Karam <minakaram.me@gmail.com>
2022-11-01 21:14:56 +0200

    02-Python-Programming/Sessions/Session-0/lab_3: Add new code

--- 02-Python-Programming/Sessions/Session-0/lab_3.py
+++ 02-Python-Programming/Sessions/Session-0/lab_3.py
@@ -5,3 +5,27 @@ print(x)
 print(y)
 print(z)
 # print(a) : ValueError: not enough values to unpack (expected 4, got 3)
+
+fruits = ("apple", "banana", "cherry")
+
+(green, yellow, red) = fruits
+
+print(green)
+print(yellow)
+print(red)
+
+fruits = ("apple", "banana", "cherry", "strawberry", "raspberry")
+
+(green, yellow, *red) = fruits
+
+print(green)
+print(yellow)
+print(red)
+
+fruits = ("apple", "mango", "papaya", "pineapple", "cherry")
+
+(green, *tropic, red) = fruits
+
+print(green)
+print(tropic)
+print(red)
