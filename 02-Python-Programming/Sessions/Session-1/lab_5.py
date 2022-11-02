5e105826c9afaa0d34251510e8cd08a95b5af754
Mina Karam <minakaram.me@gmail.com>
2022-11-02 11:13:54 +0200

    02-Python-Programming/Sessions/Session-1/lab_5: create

---
+++ 02-Python-Programming/Sessions/Session-1/lab_5.py
@@ -0,0 +1,13 @@
+my_dict = {
+    "Name" : "Mina",
+    "Age" : 25,
+    "Company" : ["ITI", "Valeo", "Core-Tech"],
+    "Job Title" : "Embedded Software",
+    "Year" : 2022
+    }
+
+print(my_dict)
+print(len(my_dict))
+
+print(my_dict["Name"])
+print(my_dict["Company"][1])
