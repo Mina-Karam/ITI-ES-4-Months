683b535a3b33396b1cd6dc7f7ce6f720b8dd8142
Mina Karam <minakaram.me@gmail.com>
2022-11-02 10:38:38 +0200

    02-Python-Programming/Sessions/Session-1/lab_3: create

---
+++ 02-Python-Programming/Sessions/Session-1/lab_3.py
@@ -0,0 +1,17 @@
+my_tuple = ("Mina", 25, "ITI", "Embedded Software", 2022)
+
+#Accessing all list values
+print(my_tuple)
+print(len(my_tuple))
+
+my_tuple = ("Mina", "Karam")
+
+print(my_tuple)
+print(len(my_tuple))
+
+my_tuple = ("Mina",)
+my_str = ("Mina")
+
+print(type(my_tuple))
+print(type(my_str))
+
