189f0bb9e1d08058ff0af902e2ee82fd404f3431
Mina Karam <minakaram.me@gmail.com>
2022-11-02 09:47:04 +0200

    02-Python-Programming/Sessions/Session-1/lab_1:

--- 02-Python-Programming/Sessions/Session-1/lab_1.py
+++ 02-Python-Programming/Sessions/Session-1/lab_1.py
@@ -1,3 +1,28 @@
-x = int("a")
+num1, num2 = input("Enter your two number: ").split(" ", 2)
+num1 = int(num1)
+num2 = int(num2)
 
-# print(x) gives error  
+#num1 = bin(num1)
+
+#num2 = input("Enter your Second number: ")
+#num2 = bin(num2)
+
+result = 0
+
+result = num1 & num2
+print(bin(result))
+
+result = num1 | num2
+print(bin(result))
+
+result = num1 ^ num2
+print(bin(result))
+
+result = ~num1
+print(bin(result))
+
+result = num1 >> 2
+print(bin(result))
+
+result = num1 << 2
+print(bin(result))
