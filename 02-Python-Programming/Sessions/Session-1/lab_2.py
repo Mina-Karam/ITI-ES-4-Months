8bac3a957bf4eebdde36f8bcbc5ea31e24773e6f
Mina Karam <minakaram.me@gmail.com>
2022-11-02 10:24:25 +0200

    02-Python-Programming/Sessions/Session-1/lab_2: create

---
+++ 02-Python-Programming/Sessions/Session-1/lab_2.py
@@ -0,0 +1,31 @@
+my_list = ["Mina", 25, "ITI", "Embedded Software", 2022]
+
+#Accessing all list values
+print(my_list)
+
+#Accessing list values
+print(my_list[2])# 5.4
+
+print(my_list[-1])# 0.7
+print(my_list[:2]) #  till element2
+
+my_list.append(5)
+
+print(my_list)
+
+x = "Valeo"
+y = "Karam"
+
+my_list.insert(3, x)
+print(my_list)
+
+my_list.remove(x)
+print(my_list)
+
+my_list.insert(1, y)
+print(my_list)
+
+my_list.extend(y)
+print(my_list)
+
+
