427b1b40ef2ef8f54b360f8dad10488d5430b88d
Mina Karam <minakaram.me@gmail.com>
2022-11-06 12:40:55 +0200

    03-Problem-Solving/00-C/00-HackerRank/Week-01/Diagonal-Difference:

--- 03-Problem-Solving/00-C/00-HackerRank/Week-01/Diagonal-Difference.c
+++ 03-Problem-Solving/00-C/00-HackerRank/Week-01/Diagonal-Difference.c
@@ -2,13 +2,9 @@ int diagonalDifference(int arr_rows, int arr_columns, int** arr)
 {
     int value_1 = 0, value_2 = 0, abs_value = 0;
     
-    for (int i = 0; i < arr_columns; i++)
-    {
-        value_1 += arr[i][i];
-    }
-    
     for (int i = 0, j = arr_columns-1; i < arr_rows, j >= 0 ; i++, j--)
     {
+        value_1 += arr[i][i];
         value_2 += arr[i][j];
     }
     
