521028dd95e94cf9ae1294f26836947740419241
Mina Karam <minakaram.me@gmail.com>
2022-11-07 19:46:43 +0200

    01-C++-Programming/Sessions/Session-2/Lab_4/main:

--- 01-C++-Programming/Sessions/Session-2/Lab_4/main.cpp
+++ 01-C++-Programming/Sessions/Session-2/Lab_4/main.cpp
@@ -4,33 +4,6 @@ using namespace std;
 
 int main() 
 {
-  vector<int> vector1;
-
-  int x{0}, value{0}, total{};
-
-  cout << "Please Enter No. of the elements to store in vector: ";
-  cin >> x;
-
-  for (int i = 0; i < x; i++)
-  {
-    cout << "Please Enter the element No. " << i+1 << " : ";
-    cin >> value;
-
-    vector1.push_back(value);
-  }
-  for (auto values : vector1)
-  {
-    total += values;
-  }
-  for (int i = 0; i < x; i++)
-  {
-    total += vector1.at(i);
-  }
-
-  cout << total << endl;
-
-  
-  
 
   return 0;
 }
